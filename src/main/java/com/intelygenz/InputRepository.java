8cfcec89f2abec9a4c5045663d615568ab8c43b9
Fernando Rodríguez <fernando.rodriguez@intelygenz.com>
2023-04-21 09:29:23 +0200

    Remove some unnecessary files

--- src/main/java/com/intelygenz/InputRepository.java
+++
@@ -1,8 +0,0 @@
-package com.intelygenz;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InputRepository extends MongoRepository<InputEntity, String> {
-
-
-}
